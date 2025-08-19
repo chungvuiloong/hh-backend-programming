a5541d52ef9dab1d67c8f272dbab0e27826062a7
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-08-19 12:36:33 +0300

    Fixed bugs and got the hello world

---
+++ homework01/README.md
@@ -0,0 +1,14 @@
+# Homework01
+
+## How to Start the Project
+
+This is a Spring Boot project. To start it, run:
+
+```bash
+./mvnw spring-boot:run
+```
+
+Or on Windows:
+```bash
+mvnw.cmd spring-boot:run
+```
--- homework01/src/main/java/fi/haagahelia/homework01/Homework01Application.java
+++ homework01/src/main/java/fi/haagahelia/homework01/Homework01Application.java
@@ -3,8 +3,10 @@ package fi.haagahelia.homework01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Homework01Application {
 
 	public static void main(String[] args) {
