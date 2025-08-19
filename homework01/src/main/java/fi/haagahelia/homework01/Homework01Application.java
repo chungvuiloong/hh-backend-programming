c19de10d90aeda7d0bcb5065c4e674e56d877d99
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-08-19 12:43:14 +0300

    Added index.

--- homework01/src/main/java/fi/haagahelia/homework01/Homework01Application.java
+++ homework01/src/main/java/fi/haagahelia/homework01/Homework01Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 @RestController
@@ -13,8 +15,14 @@ public class Homework01Application {
 		SpringApplication.run(Homework01Application.class, args);
 	}
 
+    @GetMapping("/index")
+    public String getIndexAPI() {
+        return "This is the main page!";
+    }
+    
+
     @GetMapping("/hello")
-    public String sayHello() {
+    public String getHelloAPI() {
         return "Hello, World!";
     }
 
