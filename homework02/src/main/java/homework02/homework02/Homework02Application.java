a8fe8718232ebd9abd4cad84012bf71382b15bbb
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-08-30 13:33:42 +0300

    Implement controller for greeting and update index page title

--- homework02/src/main/java/homework02/homework02/Homework02Application.java
+++ homework02/src/main/java/homework02/homework02/Homework02Application.java
@@ -2,6 +2,10 @@ package homework02.homework02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class Homework02Application {
@@ -10,4 +14,19 @@ public class Homework02Application {
 		SpringApplication.run(Homework02Application.class, args);
 	}
 
+    @Controller
+    public class MyController {
+        @RequestMapping("/index")
+            public String home() {
+            return "index";
+        }
+
+        @GetMapping("/hello")
+        public String getHelloAPI(
+            @RequestParam(name = "name", required = false, defaultValue = "") String name,
+            @RequestParam(name = "age", required = false, defaultValue = "0") int age
+        ) {
+            return "Welcome to the " + name + " " + age + "!";
+        }
+    }
 }
