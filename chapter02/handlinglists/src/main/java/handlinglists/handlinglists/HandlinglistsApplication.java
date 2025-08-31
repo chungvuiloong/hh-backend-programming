5e7bbffbab1af934ca51bf4059be4c96b4c9df5e
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-08-31 22:32:45 +0300

    Made a hello route.

---
+++ chapter02/handlinglists/src/main/java/handlinglists/handlinglists/HandlinglistsApplication.java
@@ -0,0 +1,25 @@
+package handlinglists.handlinglists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class HandlinglistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlinglistsApplication.class, args);
+	}
+
+    @Controller
+    public class MyController {
+
+
+        @GetMapping("/hello")
+        public String getHelloAPI() {
+            return "Welcome to the Haaga-Helia!";
+        }
+    }
+
+}
---
+++ chapter02/handlinglists/src/test/java/handlinglists/handlinglists/HandlinglistsApplicationTests.java
@@ -0,0 +1,13 @@
+package handlinglists.handlinglists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlinglistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
