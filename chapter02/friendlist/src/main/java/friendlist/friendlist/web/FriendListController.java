e10da1c6709c8c20343f66d1d76c3454a4622a59
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-01 13:17:13 +0300

    Refactored list of friends.

--- chapter02/friendlist/src/main/java/friendlist/friendlist/web/FriendListController.java
+++ chapter02/friendlist/src/main/java/friendlist/friendlist/web/FriendListController.java
@@ -5,20 +5,23 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import friendlist.friendlist.domain.Friend;
-
 import org.springframework.ui.Model;
 
 @Controller
 public class FriendListController {
+    List<Friend> friends = new ArrayList<>(
+        List.of(
+            new Friend("Minna", "Pellikka"),
+            new Friend("Tanja", "Bergius"),
+            new Friend("Jukka", "Juslin")
+        )
+    );
+
+
     @GetMapping("/")
     public String getFriends(Model model) {
-        List<Friend> friends = new ArrayList<>();
-        friends.add(new Friend("Minna",  "Pellikka"));
-        friends.add(new Friend("Tanja", "Bergius"));
-        friends.add(new Friend("Jukka", "Juslin"));
         model.addAttribute("friends", friends);
         return "friendsList";
-        }
+    }
 }
