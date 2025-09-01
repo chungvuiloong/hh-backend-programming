c5e15b193a3c420c13c1bab9469e0b6592141cea
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-01 13:39:30 +0300

    Remmoved commented/ unused code.

--- chapter02/friendlist/src/main/java/friendlist/friendlist/FriendlistApplication.java
+++ chapter02/friendlist/src/main/java/friendlist/friendlist/FriendlistApplication.java
@@ -2,8 +2,6 @@ package friendlist.friendlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class FriendListApplication {
@@ -11,13 +9,4 @@ public class FriendListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FriendListApplication.class, args);
 	}
-
-    // @Controller
-    // public class FriendlistController {
-    //     @GetMapping("/")
-    //     public String getFriends() {
-    //         return "friendsList";
-    //     }
-    // }
-
 }
