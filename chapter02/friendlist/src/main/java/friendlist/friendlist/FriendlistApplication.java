71c120112cbe5e4ef79a521649e6af1477de9041
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-01 09:44:42 +0300

    Made routing for the Java.

--- chapter02/friendlist/src/main/java/friendlist/friendlist/FriendlistApplication.java
+++ chapter02/friendlist/src/main/java/friendlist/friendlist/FriendlistApplication.java
@@ -2,6 +2,8 @@ package friendlist.friendlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class FriendlistApplication {
@@ -9,5 +11,13 @@ public class FriendlistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FriendlistApplication.class, args);
 	}
+
+    @Controller
+    public class FriendlistController {
+        @GetMapping("/")
+        public String getFriends() {
+            return "friendsList";
+        }
+    }
 
 }
