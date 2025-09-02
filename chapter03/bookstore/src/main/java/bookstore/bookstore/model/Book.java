7715e6b820a1cba4f2b8257f3ecefd8b9f116d4c
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-02 11:39:35 +0300

    Added import GeneratedValue and Generation Type.

--- chapter03/bookstore/src/main/java/bookstore/bookstore/model/Book.java
+++ chapter03/bookstore/src/main/java/bookstore/bookstore/model/Book.java
@@ -1,6 +1,8 @@
 package bookstore.bookstore.model;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class Book {
