2f2dfefd4228ee0ac6dfb164de9dbc2f76bdfff5
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-09 09:55:49 +0300

    Added default.

--- chapter03/bookstore/src/main/java/bookstore/bookstore/model/Book.java
+++ chapter03/bookstore/src/main/java/bookstore/bookstore/model/Book.java
@@ -15,6 +15,9 @@ public class Book {
     private String isbn;
     double price;
 
+    public Book() {
+    }
+
     public Book(String title, String author, int publicationYear, String isbn, double price) {
         this.title = title;
         this.author = author;
