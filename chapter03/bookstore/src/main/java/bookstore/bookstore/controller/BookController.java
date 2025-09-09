bb40c499cfc6a3ea8c817370fddd7240ecf580f5
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-09 09:56:11 +0300

    Added new list.

--- chapter03/bookstore/src/main/java/bookstore/bookstore/controller/BookController.java
+++ chapter03/bookstore/src/main/java/bookstore/bookstore/controller/BookController.java
@@ -1,12 +1,33 @@
 package bookstore.bookstore.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import bookstore.bookstore.model.Book;
+import ch.qos.logback.core.model.Model;
 
 @Controller
 public class BookController {
+    List<Book> books = new ArrayList<>(
+        List.of(
+            new Book("Minna", "Pellikka", 2020, "1234567890123", 29.99),
+            new Book("Tanja", "Bergius", 2019, "1234567890124", 19.99),
+            new Book("Jukka", "Juslin", 2021, "1234567890125", 39.99)
+        )
+    );
     @RequestMapping("/index")
-    public String getBooks() {
+    public String getIndex() {
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String getBooks(Model model) {
+        model.addAttribute("books", books);
+        model.addAttribute("book", new Book());
         return "index";
     }
 
