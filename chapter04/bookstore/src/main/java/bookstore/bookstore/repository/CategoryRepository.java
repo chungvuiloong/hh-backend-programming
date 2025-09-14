1f59cb61ab0e0efc4b3c40dfae12aeebf9df85ee
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-14 22:45:20 +0300

    Added repository class for the Category entity

---
+++ chapter04/bookstore/src/main/java/bookstore/bookstore/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package bookstore.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import bookstore.bookstore.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
