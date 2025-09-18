005715e9454f0b99d27105c063483219ce01d868
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-18 22:48:48 +0300

    Created REST service that return all books (JSON) a. Created a method to controller

--- chapter05/bookstore/src/main/java/bookstore/bookstore/controller/BookController.java
+++ chapter05/bookstore/src/main/java/bookstore/bookstore/controller/BookController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import bookstore.bookstore.model.Book;
 import bookstore.bookstore.model.Category;
@@ -40,6 +43,11 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         model.addAttribute("book", new Book());
         return "index";
+    }
+
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+        return (List<Book>) repository.findAll();
     }
 
     @GetMapping("/addbook")
