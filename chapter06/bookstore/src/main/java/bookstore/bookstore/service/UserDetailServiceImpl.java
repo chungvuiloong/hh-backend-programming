042a5344edb8251b001bd62710a6336f0f85aa40
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-09-29 11:41:40 +0300

    Used UserDetailService interface from powerpints. Spring Security is using it to authenticate and authorize user

---
+++ chapter06/bookstore/src/main/java/bookstore/bookstore/service/UserDetailServiceImpl.java
@@ -0,0 +1,33 @@
+package bookstore.bookstore.service;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import bookstore.bookstore.model.User;
+import bookstore.bookstore.repository.UserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final UserRepository repository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.repository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User currentUser = repository.findByUsername(username);
+
+        if (currentUser == null) {
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+
+        UserDetails user = new org.springframework.security.core.userdetails.User(username,
+                currentUser.getPasswordHash(),
+                AuthorityUtils.createAuthorityList(currentUser.getRole()));
+        return user;
+    }
+}
