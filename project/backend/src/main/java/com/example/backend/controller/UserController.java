9eb30c588d316c976ea86e481d78ae2f9ab1aff7
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-10-26 22:38:44 +0200

    Added CrossOrigin and api routing for getting info about current location.

--- project/backend/src/main/java/com/example/backend/controller/UserController.java
+++ project/backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,12 +1,37 @@
 package com.example.backend.controller;
 
 import org.springframework.web.bind.annotation.*;
+import com.example.backend.util.LocationUtil;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     
     @GetMapping("/")
     public String home() {
-        return "Welcome World";
+        System.out.println(LocationUtil.getCountryByIp());
+        return (LocationUtil.getCountryByIp() != null) ? LocationUtil.getCountryByIp() : "Unknown Country";
+    }
+
+    @GetMapping("/location/country")
+    public String getCountry() {
+        String country = LocationUtil.getCountryByIp();
+        return country != null ? country : "Unknown";
+    }
+
+    @GetMapping("/location/city")
+    public String getCity() {
+        String city = LocationUtil.getCityByIp();
+        return city != null ? city : "Unknown";
+    }
+
+    @GetMapping("/location/info")
+    public Map<String, String> getLocationInfo() {
+        Map<String, String> location = new HashMap<>();
+        location.put("country", LocationUtil.getCountryByIp());
+        location.put("city", LocationUtil.getCityByIp());
+        return location;
     }
 }
