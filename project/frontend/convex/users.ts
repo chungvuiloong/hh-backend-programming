823220f2ca093664fea260022a820eae2f4ac0f6
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-11-12 23:15:40 +0200

    Added a new function to add friend to listOfFriends for users.

--- project/frontend/convex/users.ts
+++ project/frontend/convex/users.ts
@@ -46,3 +46,35 @@ export const getUserByUserId = query({
     return user;
   },
 });
+
+export const addFriendToUser = mutation({
+  args: {
+    userID: v.string(),
+    friend: v.object({
+      fullname: v.string(),
+      firstMeet: v.string(),
+      identity: v.optional(v.string()),
+      email: v.optional(v.string()),
+      firstName: v.optional(v.string()),
+      lastName: v.optional(v.string()),
+      phoneNumber: v.optional(v.string()),
+      notesAboutFriend: v.optional(v.string()),
+    }),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("allUsers")
+      .filter((q) => q.eq(q.field("userID"), args.userID))
+      .first();
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const updatedFriendsList = [...user.listOfFriends, args.friend];
+
+    await ctx.db.patch(user._id, {
+      listOfFriends: updatedFriendsList,
+    });
+  },
+});
