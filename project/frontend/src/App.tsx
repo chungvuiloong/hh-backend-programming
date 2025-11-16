0fdce497279eaa14b5647361ffd0d3f6a4bc7db7
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-11-17 00:10:39 +0200

    Changed layout and state management..

--- project/frontend/src/App.tsx
+++ project/frontend/src/App.tsx
@@ -19,6 +19,7 @@ interface FriendFormData {
 
 function App() {
     const { isSignedIn } = useUser();
+    const [ isModalOpened, setIsModalOpened ] = React.useState<boolean>(false);
     const [ formAction, setFormAction ] = React.useState<'add' | 'edit'>('add');
     const [formData, setFormData] = useState<FriendFormData>({
         id: '',
@@ -30,29 +31,40 @@ function App() {
         notesAboutFriend: ''
     });
 
+    const toggleModal = React.useCallback((action?: 'add' | 'edit' | undefined) => {
+        if (action) setFormAction(action);
+        setIsModalOpened(!isModalOpened);
+    }, [isModalOpened]);
+
     return (
         <div className="container mx-auto p-4">
-            <Header />
-            <div className='mt-5 flex flex-col gap-5'>
-                <LocationDisplay />
-                {isSignedIn ? null : "Sign in to add new friends."}
-                <div>
-                    <div>
-                        <Button onClick={() => console.log("test")}>Add Friend</Button>
+            {isSignedIn && isModalOpened ? 
+                <Form 
+                    formAction={formAction} 
+                    setFormAction={setFormAction} 
+                    formData={formData} 
+                    setFormData={setFormData} 
+                    toggleModal={toggleModal}
+                    /> 
+                : 
+                <>
+                    <Header />            
+                    <div className='mt-5 flex flex-col gap-5'>
+                        <LocationDisplay />
+                        {isSignedIn ? null : "Sign in to add new friends."}
+                        <div>
+                            <div className='flex flex-row-reverse'>
+                                <Button path="M18 9v3m0 0v3m0-3h3m-3 0h-3m-9 4.5c0-1.657 1.343-3 3-3h4c1.657 0 3 1.343 3 3M15 7a4 4 0 11-8 0 4 4 0 018 0z" 
+                                    onClick={() => toggleModal("add")}>Add Friend
+                                </Button>
+                            </div>
+                        </div>
+                        <div className='p-4'>
+                            {isSignedIn ? <FriendList toggleModal={toggleModal} /> : ""}
+                        </div>
                     </div>
-                    {isSignedIn ? 
-                        <Form 
-                            formAction={formAction} 
-                            setFormAction={setFormAction} 
-                            formData={formData} 
-                            setFormData={setFormData} /> 
-                        : ""
-                    }
-                </div>
-                <div className='p-4'>
-                    {isSignedIn ? <FriendList /> : ""}
-                </div>
-            </div>
+                </>
+            }
         </div>
   );
 }
